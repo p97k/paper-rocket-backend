a6ed739afeb67a85ce60a7927499dd78018c25f8
parham <parham.khoshbin@digikala.com>
2024-04-01 18:23:45 +0330

    feat: add base route as checker

--- router/router.go
+++ router/router.go
@@ -3,6 +3,8 @@ package router
 import (
 	"database/sql"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"server/util"
 )
 
 var router *gin.Engine
@@ -11,10 +13,20 @@ func InitRouter(db *sql.DB) {
 	router = gin.Default()
 	router.Use(CORSMiddleware())
 
+	router.GET("/", initBaseRouter)
+
 	InitUserRoutes(db, router)
 	InitWebsocketRoutes(router)
 }
 
 func Start(address string) error {
 	return router.Run(address)
+}
+
+func initBaseRouter(c *gin.Context) {
+	c.JSON(http.StatusOK, util.Response{
+		Data:    nil,
+		Status:  http.StatusOK,
+		Message: "logout successfully",
+	})
 }
